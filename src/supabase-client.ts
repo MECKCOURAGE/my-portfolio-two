a9368e65bff098f1d0180293eb4dd7083a6d8d90
MECK COURAGE <themeckcourage07@gmail.com>
2026-04-20 03:33:19 +0100

    changes

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,9 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
+import { defaultFallbackInView } from 'react-intersection-observer';
 
-const supabase = createClient(
-  "https://fapfdvqlymoqavvdqkwy.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhcGZkdnFseW1vcWF2dmRxa3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwOTQ2NjUsImV4cCI6MjA3NjY3MDY2NX0.E_zQHEBH6WBZtcuhL0zsxtUpZyZL2fQFvQVMKSVoF4A"
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
- 
